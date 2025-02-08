117e6a55f8ce5a5cfd26c6546511b7be1029bde2
Muhammad Anas Qadri <161422002+Anas-Rajput12@users.noreply.github.com>
2025-02-09 01:20:58 +0500

    Update page.tsx

--- app/payment2/page.tsx
+++ app/payment2/page.tsx
@@ -50,10 +50,10 @@ export default function Home() {
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-slate-400 to-zinc-900">
       <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Please Pay </h1>
+        <h1 className="text-4xl font-extrabold mb-2">Select and Pay</h1>
         <h2 className="text-2xl">
-          This Payment
-          <span className="font-bold"> ${booking.totalPrice}</span>
+          Payment
+          <span className="font-bold"></span>
         </h2>
       </div>
 
